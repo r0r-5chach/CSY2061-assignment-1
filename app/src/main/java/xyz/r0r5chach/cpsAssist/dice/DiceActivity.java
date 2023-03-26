42a19f092d13a0dd8bb9173e086d0ffe614786cf
r0r-5chach <joshualukekillen@gmail.com>
2023-03-26 21:04:47 +0100

    added dice roller color background compatibility

--- app/src/main/java/xyz/r0r5chach/cpsAssist/dice/DiceActivity.java
+++ app/src/main/java/xyz/r0r5chach/cpsAssist/dice/DiceActivity.java
@@ -1,17 +1,21 @@
 package xyz.r0r5chach.cpsAssist.dice;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import xyz.r0r5chach.cpsAssist.R;
-import xyz.r0r5chach.cpsAssist.dice.BtnOnClickListener;
 
 public class DiceActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dice);
+        setTitle("Dice Roller- " + getIntent().getStringExtra("username"));
+        if (getIntent().hasExtra("background")) {
+            findViewById(R.id.dice_layout).setBackgroundColor(Color.parseColor(getIntent().getStringExtra("background")));
+        }
         findViewById(R.id.dice_roll_button).setOnClickListener(new BtnOnClickListener());
     }
 
